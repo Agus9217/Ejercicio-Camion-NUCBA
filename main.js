655b03b526fe0b18ed8fe1885ce35e186b127740
Agus9217 <justinibarrola92@gmail.com>
2022-09-21 00:01:00 -0300

    JS-Ejercicio-Camion

--- main.js
+++ main.js
@@ -38,58 +38,46 @@ const printColor = document.getElementById('truck-color')
 const button = document.getElementById('btn-color')
 const fragment = document.createDocumentFragment();
 
-
-
 const init = () => {
-    window.addEventListener('DOMContentLoaded', () => {
-        trucks.forEach((item) => {
-            const option = document.createElement('option')
-            const marca = item.marca
-            const idTruck = item.id
-            option.value = idTruck
-            option.textContent = marca
-            fragment.appendChild(option)
-            selectTruck.appendChild(fragment)
-        })
-        listen()
-    })
+  window.addEventListener('DOMContentLoaded', () => {
+    selectTruckFunction()
+    listen()
+    console.log('Ya esta todo cargado')
+  });
+}
 
+const selectTruckFunction = () => {
+  trucks.forEach((item) => {
+    const optionTruck = document.createElement('option')
+    optionTruck.value = item.id
+    optionTruck.textContent = item.marca
+    fragment.appendChild(optionTruck)
+    selectTruck.appendChild(fragment)
+    selectColorFunction(item)
+  }) 
+}
+
+const selectColorFunction = (valueItem) => {
+  valueItem.color.forEach((color) => {
+    const optionColor = document.createElement('option')
+    optionColor.textContent = color
+    optionColor.value = valueItem.id
+    fragment.appendChild(optionColor)
+    selectColor.appendChild(fragment)
+  })
 }
 
 const listen = () => {
-    selectTruck.addEventListener('change', (e) => {
-        const optionId = e.target.selectedIndex + 1
-        searchTruck(optionId)
-    })
-
+  selectTruck.addEventListener('change', (e) => {
+    const valueId = e.target.value
+    changeColor(valueId)
+  })
 }
 
-const printTruck = (truck, option) => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault()
-        printName.innerHTML = `${ truck.marca }`
-        selectColor.remove(option)
-    })
-
+const changeColor = (idTruck) => {
+  selectColor.value = idTruck 
 }
-
-const searchTruck = (valueId) => {
-    const filterTruck = trucks.find(item => item.id === valueId)
-    if(filterTruck.id !== valueId){
-        console.log('No hay nada')
-    } else {
-        filterTruck.color.forEach((color) => {
-            console.log(color)
-            const optionColor = document.createElement('option')
-            optionColor.textContent = color
-            selectColor.appendChild(optionColor)
-            printTruck(filterTruck, optionColor)
-        })
-    }
-
-}
-
-
-
 
 init()
+
+
